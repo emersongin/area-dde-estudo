74a99edb406143398498bf52a8f132463b2d848c
emersongin <emersonandrey_7@hotmail.com>
2019-08-24 12:00:46 -0300

    70%

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 window.onload = function() {
-    function getData(url, callback) {
+
+    loadHeader();
+    loadMain();
+
+    function loadHeader() {
+        loadData("data/subjects.json", createHeader);
+    }
+
+    function loadMain() {
+        loadData("data/courses.json", createMain);
+    }
+
+    function loadData(url, callback) {
         let xhttp = new XMLHttpRequest();
 
         xhttp.onreadystatechange = function() {
@@ -12,50 +24,87 @@ window.onload = function() {
         xhttp.send();
     }
 
-    function getTitle(data) {
-        let title = document.getElementById('subject-title');
-        let course = document.getElementById('subject-course');
-        let teacher = document.getElementById('subject-teacher');
+    function createHeader(data) {
+        let header = document.getElementById('header-content');
+        let subject = document.getElementById('template-header').content.cloneNode(true);
 
         data = data.shift();
 
-        title.innerHTML = data.title;
-        course.innerHTML = `<b>Curso:</b> ${data.course}`;
-        teacher.innerHTML = `<b>Prof.:</b> ${data.teacher}`;
+        subject.querySelector('#subject-title').innerHTML = data.title;
+        subject.querySelector('#subject-course').innerHTML = `<b>Curso:</b> ${data.course}`;
+        subject.querySelector('#subject-teacher').innerHTML = `<b>Prof.:</b> ${data.teacher}`;
+
+        header.appendChild(subject);
     } 
 
-    function loadTitle() {
-        getData("data/subjects.json", getTitle);
-    }
-
-    function getCourses(data) {
-        let content = document.getElementById('main-content');
+    function createMain(data) {
+        let main = document.getElementById('main-content');
         
-        data.forEach(item => {
-            let child = document.getElementById('template-course').content.cloneNode(true);
-            child.querySelector('.card-header').innerHTML = item.title;
-            child.querySelector('.card-body').id = item.tag;
-            content.appendChild(child);
+        data.forEach(course => {
+            let square = document.getElementById('template-course').content.cloneNode(true);
+            let accordion = document.getElementById('template-accordion').content.cloneNode(true);
+
+            square.querySelector('.card-header').innerHTML = course.title;
+            square.querySelector('.card-body').id = course.tag;
+
+            accordion.querySelector('#template-accordion-id').id = `accordion-${course.tag}`;
+
+            square.querySelector(`#${course.tag}`).appendChild(accordion);
+            main.appendChild(square);
         });
 
-        getData("data/links.json", getLinks);
+        loadData("data/links.json", createLinks);
     }
 
-    function getLinks(data) {
+    function createLinks(data) {
         data.forEach(link => {
-            let parent = document.querySelector(`#${link.tag}`);
-            let row = document.createElement('p');
-            row.innerHTML = link.description;
+            let idAccordion = `accordion-${link.tag}`;
+            let idTab = `tab-${link.tag}-${link.slug}`;
+            let idButton = `${idTab}-button`;
+            let idCollapse = `${idTab}-collaps`;
+            let idList = `${idTab}-list`;
 
-            parent.appendChild(row);
+            let accordion = document.querySelector(`#${idAccordion}`);
+            let tab = accordion.querySelector(`#${idTab}`);
+            
+            if (tab != null) {
+                addLink();
+            } else {
+                addTab();
+                addLink();
+            }
+
+            function addLink(){
+                let list = document.getElementById(`${idList}`);
+                let line = document.createElement('li');
+                let row = document.createElement('a');
+
+                row.innerHTML = link.name;
+                row.href = link.href;
+                row.target = "blank";                
+                
+                line.appendChild(row);
+                list.appendChild(line);
+            }
+
+            function addTab() {
+                let templateTab = document.getElementById('template-tab').content.cloneNode(true);
+
+                templateTab.querySelector('#template-tab-id').id = idTab;
+                templateTab.querySelector('#template-tab-button-id').id = idButton;
+                templateTab.querySelector('#template-tab-collapse-id').id = idCollapse;
+                templateTab.querySelector('#template-tab-list-id').id = idList;
+
+                templateTab.querySelector(`#${idButton}`).innerHTML = link.slug;
+                templateTab.querySelector(`#${idButton}`).setAttribute('data-target', `#${idCollapse}`);
+                templateTab.querySelector(`#${idButton}`).setAttribute('aria-controls', idCollapse);
+                                
+                templateTab.querySelector(`#${idCollapse}`).setAttribute('aria-labelledby', idTab);
+                templateTab.querySelector(`#${idCollapse}`).setAttribute('data-parent', `#${idAccordion}`);
+
+                accordion.appendChild(templateTab);
+            }
         });
         
     }
-
-    function loadContent() {
-        getData("data/courses.json", getCourses);
-    }
-
-    loadTitle();
-    loadContent();
 };
